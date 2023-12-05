f65fabb251c78dc4a5de99e80c5b6418de47e959
kyytaylor01 <129698620+kyytaylor01@users.noreply.github.com>
2023-12-05 14:17:44 -0600

    hide initial preview

--- public/html/index.php
+++ public/html/index.php
@@ -500,12 +500,14 @@
 
     function previewFile() {
     const preview = document.querySelector('#preview');
+    preview.style.display = 'none';
     const file = document.querySelector('input[type=file]').files[0];
     const reader = new FileReader();
 
     reader.addEventListener("load", function () {
         // convert image file to base64 string
         preview.src = reader.result;
+        preview.style.display = 'block';
     }, false);
 
     if (file) {
