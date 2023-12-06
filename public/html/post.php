3ef89724b9bcb7962c200298cb68c2b12375e316
kyytaylor01 <129698620+kyytaylor01@users.noreply.github.com>
2023-12-06 17:39:46 -0600

    Update post.php

--- public/html/post.php
+++ public/html/post.php
@@ -21,24 +21,14 @@
     $content = $_POST['content'];
     $user = $_SESSION['username'];
 
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["photo"]["name"]);
-
-    if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["photo"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-
-    $sql = "INSERT INTO post (username, title, content, photo) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO post (username, title, content) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssss", $user, $title, $content, $target_file);
+    $stmt->bind_param("sss", $user, $title, $content);
 
     if ($stmt->execute()) {
         echo "New post created successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
     $conn->close();
 ?>
